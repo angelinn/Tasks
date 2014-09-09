f4ba5fd794c180e76332ef7b96182b3f1344b651
betrakiss <betrakiss@hotmail.com>
2014-09-09 13:52:30 +0300

    Fixing out of range error.

--- Hack-Bulgaria/ABCheck.cpp
+++ Hack-Bulgaria/ABCheck.cpp
@@ -6,7 +6,7 @@ bool ABCheck(const char* str)
 
 	for(int i=0; i <= stringSize-1; ++i)
 		if(str[i] == 'a')
-			if(str[i+4] == 'b')
+			if(i+4 <= stringSize-1 && str[i+4] == 'b')
 				return true;
 
 	return false;
